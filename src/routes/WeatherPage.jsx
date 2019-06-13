45f426c55d39779c7f07a756c0d5a09a9156b56f
cowkeys <cowkeys@outlook.com>
2019-06-13 20:04:01 +0800

    finish v1 static

--- src/routes/WeatherPage.jsx
+++ src/routes/WeatherPage.jsx
@@ -45,7 +45,35 @@ const WeatherPage = (props) => {
             <p className={styles.weatherrange}>21℃ ~ 26℃</p>
           </div>
       </div>
+
       <div>
+        <table className={styles.weathertable}>
+          <tr>
+            <td>Web</td>
+            <td>Thu</td>
+            <td>Fri</td>
+            <td>Sat</td>
+            <td>Sun</td>
+          </tr>
+          <tr>
+            <td><img className={styles.weathersubimg} src={require('../assets/weather/12_Rain.svg')}/></td>
+            <td><img className={styles.weathersubimg} src={require('../assets/weather/7_Mixed Snow And Sleet.svg')}/></td>
+            <td><img className={styles.weathersubimg} src={require('../assets/weather/11_Showers.svg')}/></td>
+            <td><img className={styles.weathersubimg} src={require('../assets/weather/23_Blustery.svg')}/></td>
+            <td><img className={styles.weathersubimg} src={require('../assets/weather/4_Thunderstorms.svg')}/></td>
+          </tr>
+          <tr>
+          <td className={styles.weathersubrange}>21~26℃</td>
+          <td className={styles.weathersubrange}>21~26℃</td>
+          <td className={styles.weathersubrange}>21~26℃</td>
+          <td className={styles.weathersubrange}>21~26℃</td>
+          <td className={styles.weathersubrange}>21~26℃</td>
+          </tr>
+        </table>
+      </div>
+      <div className={styles.addiv}>
+        <div className={styles.adone}>
+        </div>
       </div>
     </div>
   </div>
