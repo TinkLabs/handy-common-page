75af6c8f986eb5281f12cdbb6c2d4b2c3b3d888f
cowkeys <cowkeys@outlook.com>
2019-06-17 14:45:00 +0800

    add spec

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, Switch,IndexRoute } from 'dva/router';
+import { BrowserRouter as Router, Route, Switch,IndexRoute } from 'dva/router';
 import IndexPage from './routes/IndexPage';
 import WeatherPage from './routes/WeatherPage.jsx';
 import JR from './routes/JR.jsx';
