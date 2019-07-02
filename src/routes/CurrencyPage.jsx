777243b9e562b127c2056dbf1aaee2e028319c5c
cowkeys <cowkeys@outlook.com>
2019-07-02 16:10:27 +0800

    fix title and debug

--- src/routes/CurrencyPage.jsx
+++ src/routes/CurrencyPage.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-// import TopBar from '../components/topBar/TopBar';
 import CurrencyPanel from '../components/currencyPanel/currency';
 import styles from './CurrencyPage.css';
 import { connect } from 'dva';
 import {formatCurrencyDate} from '../utils/common';
 import DebugIt from '../components/mydebug/DebugIt';
 import AdUnit from '../components/AmpAD';
-// import GPTPanel from '../components/GPTPanel/index';
-// import {AdCurrencyPath,backtohp} from '../utils/env';
 
 const CurrencyPage = (props) => {
   var onReturn = () => {
