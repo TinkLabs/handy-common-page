16ce4123a1fdd995fb71b5a2590957ac12eea493
Xiaoguang Liu <vincexgliu@gmail.com>
2019-07-25 11:10:58 +0800

    suica v1

--- src/routes/JR.jsx
+++ src/routes/JR.jsx
@@ -5,6 +5,10 @@ import { connect } from "dva";
 import DebugIt from "../components/mydebug/DebugIt";
 
 const JR = props => {
+  let textInput1 = React.createRef();
+  let textInput2 = React.createRef();
+  let textInput3 = React.createRef();
+
   var onReturn = () => {
     window.location.href = "https://www.hiinc.com/ja/";
   };
@@ -12,7 +16,7 @@ const JR = props => {
   const checkChar = value => {
     const reg = /^[A-Za-z]{0,3}$/;
     return reg.test(value);
-  }
+  };
 
   const validate = (data, index) => {
     // console.log("get ",props.suica)
@@ -27,11 +31,10 @@ const JR = props => {
         return;
       }
     } else {
-      console.log()
       if (!checkChar(data.target.value)) {
-        return
+        return;
       } else {
-        data.target.value = data.target.value.toUpperCase()
+        data.target.value = data.target.value.toUpperCase();
       }
     }
 
@@ -49,16 +52,24 @@ const JR = props => {
   };
 
   var onValueChange = data => {
-    console.log(data.target, 29999);
     switch (data.target.name) {
       case "name0":
         validate(data, 0);
+        if (data.target.value.length === 3) {
+          textInput1.current.focus();
+        }
         break;
       case "name1":
         validate(data, 1);
+        if (data.target.value.length === 3) {
+          textInput2.current.focus();
+        }
         break;
       case "name2":
         validate(data, 2);
+        if (data.target.value.length === 3) {
+          textInput3.current.focus();
+        }
         break;
       case "name3":
         validate(data, 3);
@@ -72,16 +83,21 @@ const JR = props => {
     if (props.btntext !== "OK") {
       return;
     }
-    if (
-      Number(props.suica) < 100000000000 ||
-      Number(props.suica) > 999999999999
-    ) {
+    const temp = props.suica0 + props.suica1 + props.suica2 + props.suica3;
+
+    if (temp.length !== 12) {
       props.dispatch({
         type: "jr/save",
         payload: { wrong: true }
       });
       return;
     }
+
+    props.dispatch({
+      type: "jr/save",
+      payload: { suica: temp }
+    });
+
     props.dispatch({
       type: "jr/save",
       payload: { btntext: "waiting..." }
@@ -89,74 +105,106 @@ const JR = props => {
 
     props.dispatch({
       type: "jr/validcode",
-      payload: { suica: props.suica }
+      payload: { suica: temp }
     });
   };
 
   return (
     <div>
-      <div className={styles.jrbackground}>
-        {/* eslint-disable-next-line jsx-a11y/alt-text */}
-        <img
-          className={styles.jrtext}
-          src={require("../assets/jr/top_text.svg")}
-        />
-      </div>
-
-      <div className={styles.jrform}>
-        <p className={styles.jrsubtitle}>Enter Welcome Suica Number here</p>
-        {props.wrong ? (
-          <span className={styles.failedstatus}>
-            Please Check Your Suica Number
-          </span>
-        ) : props.num === 0 ? (
-          <span />
-        ) : props.success ? (
-          <span className={styles.okstatus}>Success!</span>
-        ) : (
-          <span className={styles.failedstatus}>Failed!</span>
-        )}
-
-        <div>
-          <div className={styles.suicaContainer}>
-            <input
-              value={props.suica0}
-              onChange={onValueChange}
-              className={styles.number}
-              type="text"
-              name="name0"
-            />
-            <input
-              value={props.suica1}
-              onChange={onValueChange}
-              className={styles.number}
-              type="text"
-              name="name1"
-            />
-            <input
-              value={props.suica2}
-              onChange={onValueChange}
-              className={styles.number}
-              type="text"
-              name="name2"
-            />
-            <input
-              value={props.suica3}
-              onChange={onValueChange}
-              className={styles.number}
-              type="text"
-              name="name3"
+      {!(!props.wrong && props.num !== 0 && props.success) && (
+        <div className="suica-input">
+          <div className={styles.jrbackground}>
+            {/* eslint-disable-next-line jsx-a11y/alt-text */}
+            <img
+              className={styles.jrtext}
+              src={require("../assets/jr/top_text.svg")}
             />
           </div>
 
-          <input
-            onClick={onSubmit}
-            className={props.btntext === "OK" ? styles.btn : styles.waitbtn}
-            type="submit"
-            value={props.btntext}
-          />
+          <div className={styles.jrform}>
+            <p className={styles.jrsubtitle}>Enter Welcome Suica Number here</p>
+            {props.wrong ? (
+              <span className={styles.failedstatus}>
+                Please Check Your Suica Number
+              </span>
+            ) : props.num === 0 ? (
+              <span />
+            ) : props.success ? (
+              <span className={styles.okstatus}>Success!</span>
+            ) : (
+              <span className={styles.failedstatus}>Failed!</span>
+            )}
+
+            <div>
+              <div className={styles.suicaContainer}>
+                <input
+                  value={props.suica0}
+                  onChange={onValueChange}
+                  className={styles.number}
+                  type="text"
+                  name="name0"
+                />
+                <input
+                  value={props.suica1}
+                  onChange={onValueChange}
+                  className={styles.number}
+                  type="text"
+                  name="name1"
+                  ref={textInput1}
+                />
+                <input
+                  value={props.suica2}
+                  onChange={onValueChange}
+                  className={styles.number}
+                  type="text"
+                  name="name2"
+                  ref={textInput2}
+                />
+                <input
+                  value={props.suica3}
+                  onChange={onValueChange}
+                  className={styles.number}
+                  type="text"
+                  name="name3"
+                  ref={textInput3}
+                />
+              </div>
+
+              <input
+                onClick={onSubmit}
+                className={props.btntext === "OK" ? styles.btn : styles.waitbtn}
+                type="submit"
+                value={props.btntext}
+              />
+            </div>
+          </div>
         </div>
-      </div>
+      )}
+      {!props.wrong && props.num !== 0 && props.success && (
+        <div className="suica-thx">
+          <div className={styles.jrThxBackground}>
+            {/* eslint-disable-next-line jsx-a11y/alt-text */}
+            <img
+              className={styles.jrtext}
+              src={require("../assets/jr/thankyou_text.svg")}
+            />
+          </div>
+
+          <div className={styles.jrform}>
+            <p className={styles.jrThxSubtitle}>Consumer Feedback Survey</p>
+            <div className={styles.iframeContainer}>
+              <iframe
+                title="survey"
+                width="100%"
+                height="100%"
+                frameBorder="0"
+                allowtransparency="true"
+                src="https://www.surveymonkey.com/r/T5WQ6DX"
+              />
+            </div>
+          </div>
+        </div>
+      )}
 
       <div className={styles.footer}>
         <div>
