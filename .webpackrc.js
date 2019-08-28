4c60b8c5a4556f76f7b6943efab5a616343e6c82
Xiaoguang Liu <vincexgliu@gmail.com>
2019-08-28 11:29:15 +0800

    add hash and codesplit

--- .webpackrc.js
+++ .webpackrc.js
@@ -2,17 +2,21 @@ export default {
   entry: "src/index.js",
   define: {
     "process.env": {
-      aaaaa: 0
+      aaaaa: 0,
     },
     /*cannot set NODE_ENV for userDefined*/
     "process.env.NODE_ENV": process.env.NODE_ENV,
     "process.env.API_ENV": process.env.API_ENV,
-    "process.env.APP_ENV": process.env.APP_ENV
+    "process.env.APP_ENV": process.env.APP_ENV,
+  },
+  hash: true,
+  html: {
+    template: "./src/index.ejs",
   },
   extraBabelPlugins: [
     [
       "import",
-      { libraryName: "antd-mobile", libraryDirectory: "es", style: true }
-    ]
-  ]
+      { libraryName: "antd-mobile", libraryDirectory: "es", style: true },
+    ],
+  ],
 };
